3b9f13a802d64f90917861a13b326918fefc7656
YunShengHung <153179329+YunShengHung@users.noreply.github.com>
2023-12-07 20:43:45 +0800

    update: helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Todo: 寫一個印出"Hello World"的程式
 int main() {
-  cout << "Hello World" << endl; 
+  for(int i = 0; i < 10; i++)
+    cout << "Hello World" << endl; 
   return 0;
 }
